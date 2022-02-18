fb31d6a62bf0e4f013d7617f037f6d3a17e752a7
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2022-02-18 21:44:20 +0300

    отредактировал нажатие на ссылку

--- scripts/clickToButton.js
+++ scripts/clickToButton.js
@@ -1,9 +1,10 @@
 const allCorses = document.querySelectorAll('.card');
 
-function clickToButton(e) {
+function clickToButton() {
   allCorses.forEach(corse => {
     const button = corse.querySelector('.button');
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
       if(button.classList.contains('button_type_transparent')) {
         button.classList.remove('button_type_transparent');
         button.classList.add('button_type_filled');
